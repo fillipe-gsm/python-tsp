0fdc60ed216d51e917686e94dbe2907d3f9a8139
Fillipe Goulart <fillipe.mendes@loggi.com>
2020-06-22 19:11:24 -0300

    Copy distance matrix in case of open tsp to prevent messing with external data

--- python_tsp/brute_force.py
+++ python_tsp/brute_force.py
@@ -1,13 +1,14 @@
 """Module with a brute force TSP solver"""
 
 from itertools import permutations
+from typing import List, Tuple
 
 import numpy as np
 
 
 def solve_tsp_brute_force(
     distance_matrix: np.ndarray, open_tsp: bool = False
-):
+) -> Tuple[List, float]:
     """Solve TSP with a brute force approach
 
     Parameters
@@ -40,6 +41,8 @@ def solve_tsp_brute_force(
     # Get all points from 1 to n (fix 0 as mentioned in the notes)
     points = range(1, distance_matrix.shape[0])
     if open_tsp:
+        # Copy the matrix to prevent messing with external data
+        distance_matrix = distance_matrix.copy()
         distance_matrix[:, 0] = 0
 
     best_distance = np.inf
