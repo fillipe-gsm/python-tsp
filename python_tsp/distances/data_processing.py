7897ecd8dc51b4605128bd5490a960b3a00ca528
Fillipe Goulart <fillipe.mendes@loggi.com>
2022-06-11 10:52:34 -0300

    Move distance input processing into its own module

---
+++ python_tsp/distances/data_processing.py
@@ -0,0 +1,20 @@
+"""Common data processing tasks between all distances"""
+from typing import Optional, Tuple
+
+import numpy as np
+
+
+def process_input(
+    sources: np.ndarray, destinations: Optional[np.ndarray] = None
+) -> Tuple[np.ndarray, np.ndarray]:
+    """Pre-process input
+    This function ensures ``sources`` and ``destinations`` have at least two
+    dimensions, and if ``destinations`` is `None`, set it equal to ``sources``.
+    """
+    if destinations is None:
+        destinations = sources
+
+    sources = np.atleast_2d(sources)
+    destinations = np.atleast_2d(destinations)
+
+    return sources, destinations
