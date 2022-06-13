381b3a6ca67c5a4eba06d071bd735d450aca2835
Fillipe Goulart <fillipe.mendes@loggi.com>
2022-06-13 13:34:19 -0300

    Ignore non-imported modules
    Update coverage version

--- python_tsp/distances/__init__.py
+++ python_tsp/distances/__init__.py
@@ -2,6 +2,6 @@
 ``distances``
 =============
 """
-from .euclidean_distance import euclidean_distance_matrix
-from .great_circle_distance import great_circle_distance_matrix
-from .tsplib_distance import tsplib_distance_matrix
+from .euclidean_distance import euclidean_distance_matrix  # noqa: F401
+from .great_circle_distance import great_circle_distance_matrix  # noqa: F401
+from .tsplib_distance import tsplib_distance_matrix  # noqa: F401
