952c5d3a28edf024803f32bb5fbfb55f84641ae3
Fillipe Goulart <fillipe.mendes@loggi.com>
2022-12-06 17:26:17 -0300

    Add black formatter
    Update flake8

--- .scripts/checkup_scripts.sh
+++ .scripts/checkup_scripts.sh
@@ -2,7 +2,7 @@
 poetry run autopep8 --recursive --aggressive --in-place .
 
 # Check other requirements of PEP8
-poetry run flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
+poetry run flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics --ignore=E203,W503
 # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
 poetry run flake8 . --count --exit-zero --max-complexity=10 --max-line-length=79 --statistics
 
