a49fa659cc50bd7164a8cd0bbfb9987e2a821646
Luan <luan.moraes@loggi.com>
2023-07-24 11:12:35 -0300

    refac: refactor Node class and add NodePriorityQueue class

--- python_tsp/exact/branch_and_bound.py
+++ python_tsp/exact/branch_and_bound.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from heapq import heappop, heappush
 from typing import List, Tuple
 
 import numpy as np
@@ -5,94 +9,304 @@ import numpy as np
 INF = np.iinfo(int).max
 
 
-def compute_reduced_matrix(matrix: np.ndarray) -> Tuple[np.ndarray, int]:
+@dataclass
+class Node:
     """
-    Compute the reduced matrix and the total reduction cost.
+    Node class representing a node in a graph traversal algorithm.
 
-    Parameters
+    Attributes
     ----------
-    matrix : np.ndarray
-        Input matrix containing integer values.
+    level : int
+        The level or depth of the node in the graph traversal.
 
-    Returns
+    index : int
+        The index or vertex associated with the node.
+
+    path : List[int]
+        The path to the node, represented as a list of vertex indices.
+
+    cost : int
+        The cost or evaluation value of the node, used for
+        priority in the traversal.
+
+    reduced_matrix : np.ndarray
+        The reduced matrix representing the state of the graph after reduction
+        operations during traversal.
+
+    Methods
     -------
-    Tuple[np.ndarray, int]
-        A tuple containing the reduced matrix and the total reduction
-        cost.
+    compute_reduced_matrix(matrix: np.ndarray) -> Tuple[np.ndarray, int]:
+        Compute the reduced matrix and the total reduction cost.
+
+    root_from_cost_matrix(cost_matrix: np.ndarray) -> Node:
+        Create the root node from the cost matrix using reduction.
+
+    from_parent_node(parent_node: Node, node_index: int) -> Node:
+        Create a new node based on a parent node and a specified index.
+
+    __lt__(self: Node, other: Node) -> bool:
+        Compare nodes based on their cost values.
 
     Notes
     -----
-    - The input matrix is expected to contain integer values, where INF
-    (maximum integer value) represent an "infinite" value or an invalid
-    entry in the matrix.
-    - The reduction process involves subtracting the minimum value from
-    each row and column of the matrix when the minimum value is not INF,
-    effectively reducing the matrix size.
-    - If a row or column contains only INF values, it is not considered
-    in the reduction process.
-    - The total reduction cost is the sum of the minimum values removed
-    from the rows and columns during reduction.
+    The Node class represents a vertex in a graph traversal algorithm. It is
+    used to store the necessary information for searching and traversing a
+    graph, such as the level, index, path, cost, and reduced matrix.
+
+    The class includes three class methods for creating nodes:
+    - compute_reduced_matrix: Computes the reduced matrix and the total
+      reduction cost based on an input matrix. This is used for reducing the
+      graph during traversal to improve efficiency.
+    - root_from_cost_matrix: Creates the root node for the graph traversal
+      based on an input cost matrix. The root node serves as the starting
+      point of the search.
+    - from_parent_node: Creates a new node based on a parent node and a
+    specified index (vertex). This is used to explore the graph and generate
+    new nodes during the traversal.
+
+    The __lt__ method is implemented to allow comparison of nodes based
+    on their cost values, which is essential for the priority queue operations
+    in graph traversal algorithms.
+
+    Note that this class assumes the existence of a constant INF
+    (maximum integer value) representing an "infinite" value or an invalid
+    entry in the matrix, which is used in the computation of reduced matrices.
     """
-    mask = matrix != INF
-    reduced_matrix = np.copy(matrix)
 
-    min_rows = reduced_matrix.min(axis=1, keepdims=True)
-    min_rows[min_rows == INF] = 0
-    if np.any(min_rows != 0):
-        reduced_matrix = np.where(
-            mask, reduced_matrix - min_rows, reduced_matrix
+    level: int
+    index: int
+    path: List[int]
+    cost: int
+    reduced_matrix: np.ndarray
+
+    @staticmethod
+    def compute_reduced_matrix(matrix: np.ndarray) -> Tuple[np.ndarray, int]:
+        """
+        Compute the reduced matrix and the total reduction cost.
+
+        Parameters
+        ----------
+        matrix : np.ndarray
+            Input matrix containing integer values.
+
+        Returns
+        -------
+        Tuple[np.ndarray, int]
+            A tuple containing the reduced matrix and the total reduction
+            cost.
+
+        Notes
+        -----
+        - The input matrix is expected to contain integer values, where INF
+        (maximum integer value) represent an "infinite" value or an invalid
+        entry in the matrix.
+        - The reduction process involves subtracting the minimum value from
+        each row and column of the matrix when the minimum value is not INF,
+        effectively reducing the matrix size.
+        - If a row or column contains only INF values, it is not considered
+        in the reduction process.
+        - The total reduction cost is the sum of the minimum values removed
+        from the rows and columns during reduction.
+        """
+        mask = matrix != INF
+        reduced_matrix = np.copy(matrix)
+
+        min_rows = reduced_matrix.min(axis=1, keepdims=True)
+        min_rows[min_rows == INF] = 0
+        if np.any(min_rows != 0):
+            reduced_matrix = np.where(
+                mask, reduced_matrix - min_rows, reduced_matrix
+            )
+
+        min_cols = reduced_matrix.min(axis=0, keepdims=True)
+        min_cols[min_cols == INF] = 0
+        if np.any(min_cols != 0):
+            reduced_matrix = np.where(
+                mask, reduced_matrix - min_cols, reduced_matrix
+            )
+
+        return reduced_matrix, min_rows.sum() + min_cols.sum()
+
+    @classmethod
+    def root_from_cost_matrix(
+        cls,
+        cost_matrix: np.ndarray,
+    ) -> Node:
+        """
+        Create the root node from the cost matrix using reduction.
+
+        Parameters
+        ----------
+        cost_matrix : np.ndarray
+            The input cost matrix containing integer values.
+
+        Returns
+        -------
+        Node
+            The root node with the computed reduction and cost.
+
+        Notes
+        -----
+        This method creates the root node for the graph traversal by performing
+        the following steps:
+        1. Compute the reduced matrix and the total reduction cost using the
+           `compute_reduced_matrix` static method.
+        2. Initialize the root node with level 0, index 0, and the computed
+           reduction cost.
+        3. Set the reduced matrix and path attributes of the root node based
+           on the computed values.
+        4. The path of the root node is initialized with only the starting
+           vertex, which is 0.
+
+        The root node represents the starting point of the graph traversal and
+        serves as the initial state for the search algorithm.
+        """
+        _reduced_matrix, _reduction_cost = cls.compute_reduced_matrix(
+            matrix=cost_matrix
+        )
+        return cls(
+            level=0,
+            index=0,
+            path=[0],
+            cost=_reduction_cost,
+            reduced_matrix=_reduced_matrix,
         )
 
-    min_cols = reduced_matrix.min(axis=0, keepdims=True)
-    min_cols[min_cols == INF] = 0
-    if np.any(min_cols != 0):
-        reduced_matrix = np.where(
-            mask, reduced_matrix - min_cols, reduced_matrix
+    @classmethod
+    def from_parent_node(
+        cls,
+        parent_node: Node,
+        node_index: int,
+    ) -> Node:
+        """
+        Create a new node based on a parent node and a specified index.
+
+        Parameters
+        ----------
+        parent_node : Node
+            The parent node from which to create the new node.
+        node_index : int
+            The index or vertex associated with the new node.
+
+        Returns
+        -------
+        Node
+            A new node based on the parent node and the specified index.
+
+        Notes
+        -----
+        This method creates a new node by performing the following steps:
+        1. Create a copy of the parent node's reduced matrix.
+        2. Set all entries in the row and column corresponding to the parent
+           node's index to "infinite" (INF) to prevent revisiting the same
+           vertex.
+        3. Set the cost of reaching the new node as the sum of the parent
+           node's cost, the reduction cost of the modified matrix, and the cost
+           of moving from the parent node's index to the new node's index.
+        4. Create a new path for the new node by copying the parent node's path
+           and appending the new node's index.
+
+        This method is used to explore the graph and generate new nodes
+        during a graph traversal algorithm.
+        """
+        _cost_matrix = np.copy(parent_node.reduced_matrix)
+        _cost_matrix[parent_node.index, :] = INF
+        _cost_matrix[:, node_index] = INF
+        _cost_matrix[node_index][0] = INF
+        _reduced_matrix, _reduction_cost = cls.compute_reduced_matrix(
+            matrix=_cost_matrix
+        )
+        _cost = (
+            parent_node.cost
+            + _reduction_cost
+            + parent_node.reduced_matrix[parent_node.index][node_index]
+        )
+        return cls(
+            level=parent_node.level + 1,
+            index=node_index,
+            path=parent_node.path[:] + [node_index],
+            cost=_cost,
+            reduced_matrix=_reduced_matrix,
         )
 
-    return reduced_matrix, min_rows.sum() + min_cols.sum()
+    def __lt__(self: Node, other: Node):
+        """
+        Compare nodes based on their cost values.
 
+        Parameters
+        ----------
+        other : Node
+            Another node to compare with.
 
-class Node:
-    def __init__(self, parent_node, vertex) -> None:
-        # stores the reduced matrix
-        self.reduced_matrix: np.ndarray = None
-
-        # stores node cost lower bound
-        self.cost: int = INF
-
-        # stores the current city number
-        self.vertex: int = vertex
-
-        # stores the tour path
-        self.path: List[int] = []
-
-        # stores the total number of cities visited so far
-        self.level: int = 0
-
-        # update path, level, reduced matrix and node cost
-        # based on parent node.
-        if parent_node:
-            node_cost_matrix = np.copy(parent_node.reduced_matrix)
-            node_cost_matrix[parent_node.vertex, :] = INF
-            node_cost_matrix[:, self.vertex] = INF
-            node_cost_matrix[self.vertex][0] = INF
-            reduced_matrix, reduction_cost = compute_reduced_matrix(
-                matrix=node_cost_matrix
-            )
-            self.reduced_matrix = reduced_matrix
-            self.cost = (
-                parent_node.cost
-                + reduction_cost
-                + parent_node.reduced_matrix[parent_node.vertex][self.vertex]
-            )
-            self.path = parent_node.path
-            self.level = parent_node.level + 1
-
-        self.path.append(self.vertex)
-
-    def __lt__(self, other):
+        Returns
+        -------
+        bool
+            True if the current node's cost is less than the other node's cost,
+            False otherwise.
+        """
         return self.cost < other.cost
+
+
+@dataclass
+class NodePriorityQueue:
+    """
+    Priority Queue implementation for managing Node objects.
+
+    Attributes
+    ----------
+    _container : List[Node]
+        The internal container to store the Node objects in a
+        heap-based priority queue. Default is an empty list.
+
+    Methods
+    -------
+    empty() -> bool:
+        Check if the priority queue is empty.
+
+    push(item: Node) -> None:
+        Push a Node object into the priority queue with
+        respect to its priority.
+
+    pop() -> Node:
+        Pop the Node object with the highest priority from the priority queue.
+    """
+
+    _container: List[Node] = field(default_factory=list)
+
+    @property
+    def empty(self) -> bool:
+        """
+        Check if the priority queue is empty.
+
+        Returns
+        -------
+        bool
+            True if the priority queue is empty, False otherwise.
+        """
+        return not self._container
+
+    def push(self, item: Node) -> None:
+        """
+        Push a Node object into the priority queue with
+        respect to its priority.
+
+        Parameters
+        ----------
+        item : Node
+            The Node object to be inserted into the priority queue.
+        """
+        heappush(self._container, item)
+
+    def pop(self) -> Node:
+        """
+        Pop the Node object with the highest priority from the priority queue.
+
+        Returns
+        -------
+        Node
+            The Node object with the highest priority.
+        """
+        return heappop(self._container)
 
 
 def solve_tsp_branch_and_bound(
--- tests/exact/test_branch_and_bound.py
+++ tests/exact/test_branch_and_bound.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pytest
 
-from python_tsp.exact.branch_and_bound import Node, compute_reduced_matrix
+from python_tsp.exact.branch_and_bound import Node, NodePriorityQueue
 
 INF = np.iinfo(int).max
 
 
 @pytest.fixture
 def matrix():
+    """A cost matrix"""
     return np.array(
         [
             [INF, 20, 30, 10, 11],
@@ -21,6 +22,7 @@ def matrix():
 
 @pytest.fixture
 def reduced_matrix():
+    """Reduced matrix corresponding to the cost matrix"""
     return np.array(
         [
             [INF, 10, 17, 0, 1],
@@ -34,15 +36,15 @@ def reduced_matrix():
 
 def test_compute_reduced_matrix(matrix, reduced_matrix):
     """
-    Test the 'compute_reduced_matrix' function with different matrices.
+    Test the 'Node.compute_reduced_matrix' function with different matrices.
 
-    This test verifies the correctness of the 'compute_reduced_matrix'
+    This test verifies the correctness of the 'Node.compute_reduced_matrix'
     function for both valid and reduced matrices.
 
     Test steps:
     1. For each (request_matrix, expected_reduced_matrix, expected_cost)
     tuple in the test cases:
-        a. Call the 'compute_reduced_matrix' function with the
+        a. Call the 'Node.compute_reduced_matrix' function with the
         'request_matrix'.
         b. Check if the resulting 'response_matrix' is equal to the
         'expected_reduced_matrix', verifying that the function performs the
@@ -62,9 +64,9 @@ def test_compute_reduced_matrix(matrix, reduced_matrix):
             0,
         ),  # Already reduced matrix should remain unchanged with a cost of 0.
     ]:
-        # Step 1a: Call the 'compute_reduced_matrix'
+        # Step 1a: Call the 'Node.compute_reduced_matrix'
         # function with the 'request_matrix'.
-        response_matrix, response_cost = compute_reduced_matrix(
+        response_matrix, response_cost = Node.compute_reduced_matrix(
             matrix=request_matrix
         )
 
@@ -79,14 +81,14 @@ def test_compute_reduced_matrix(matrix, reduced_matrix):
 
 def test_compute_reduced_matrix_with_invalid_matrices():
     """
-    Test the 'compute_reduced_matrix' function with invalid matrices.
+    Test the 'Node.compute_reduced_matrix' function with invalid matrices.
 
     The function should handle invalid matrices correctly by not reducing
     any rows or columns, and the total reduction cost should be zero.
 
     Test steps:
     1. Create an invalid matrix filled with INF values.
-    2. Pass the invalid matrix to the 'compute_reduced_matrix' function.
+    2. Pass the invalid matrix to the 'Node.compute_reduced_matrix' function.
     3. Check if the resulting 'response_matrix' is equal to the input
     'invalid_matrix', indicating that no reduction was performed.
     4. Check if the 'response_cost' is zero, as no reduction was done.
@@ -95,8 +97,9 @@ def test_compute_reduced_matrix_with_invalid_matrices():
     # Step 1: Create an invalid matrix filled with INF values.
     invalid_matrix = np.full((5, 5), INF)
 
-    # Step 2: Pass the invalid matrix to the 'compute_reduced_matrix' function.
-    response_matrix, response_cost = compute_reduced_matrix(
+    # Step 2: Pass the invalid matrix to the
+    # 'Node.compute_reduced_matrix' function.
+    response_matrix, response_cost = Node.compute_reduced_matrix(
         matrix=invalid_matrix
     )
 
@@ -108,27 +111,69 @@ def test_compute_reduced_matrix_with_invalid_matrices():
     assert response_cost == 0
 
 
-def test_create_root_node(matrix, reduced_matrix):
-    response_node = Node(parent_node=None, vertex=0)
-    response_node.reduced_matrix, response_node.cost = compute_reduced_matrix(
-        matrix=matrix
-    )
+def test_create_node_from_cost_matrix(matrix, reduced_matrix):
+    """
+    Checks if the `Node.root_from_cost_matrix` method correctly creates a
+    new Node as the root node for a graph traversal, based on the provided
+    cost matrix. It verifies if the attributes of the created node match the
+    expected values.
+    """
+    response_node = Node.root_from_cost_matrix(cost_matrix=matrix)
 
-    assert np.all(response_node.reduced_matrix == reduced_matrix)
-    assert response_node.cost == 25
     assert response_node.level == 0
-    assert response_node.vertex == 0
+    assert response_node.index == 0
+    assert response_node.cost == 25
+    assert np.all(response_node.reduced_matrix == reduced_matrix)
     assert response_node.path == [0]
 
 
-@pytest.mark.parametrize("vertex,expected_cost", [(1, 35), (2, 53), (4, 31)])
-def test_create_child_node(matrix, vertex, expected_cost):
-    root = Node(parent_node=None, vertex=0)
-    root.reduced_matrix, root.cost = compute_reduced_matrix(matrix=matrix)
-    response_child_node = Node(parent_node=root, vertex=vertex)
+@pytest.mark.parametrize(
+    "node_index, node_expected_cost", [(1, 35), (2, 53), (3, 25), (4, 31)]
+)
+def test_create_child_node(matrix, node_index, node_expected_cost):
+    """
+    Checks if the `Node.from_parent_node` method correctly creates a
+    new child Node based on a parent Node and a specified index. It
+    verifies if the attributes of the created child Node match the expected
+    values.
 
-    assert root < response_child_node
-    assert response_child_node.cost == expected_cost
-    assert response_child_node.level == 1
-    assert response_child_node.vertex == vertex
-    assert response_child_node.path == [root.vertex, vertex]
+    The test creates a root Node using the provided cost matrix, and then
+    generates a new child Node based on the root Node and the specified
+    node_index. The child Node's attributes are compared with the
+    expected values.
+    """
+    root = Node.root_from_cost_matrix(cost_matrix=matrix)
+    response_node = Node.from_parent_node(
+        parent_node=root, node_index=node_index
+    )
+
+    assert response_node.level == 1
+    assert response_node.index == node_index
+    assert response_node.cost == node_expected_cost
+    assert response_node.path == [root.index, response_node.index]
+
+
+def test_nodes_priority_queue(matrix):
+    """
+    Checks the functionality of the NodePriorityQueue by creating a priority
+    queue and adding child nodes to it based on the root node. It then
+    verifies if the queue is not empty and if the first node popped from the
+    queue has the expected cost value.
+
+    The test ensures that the priority queue correctly maintains the order
+    of Node objects based on their priority (cost) and pops the node with
+    the highest priority (lowest cost) first.
+    """
+    pq = NodePriorityQueue()
+    node_indices = range(len(matrix))
+    root = Node.root_from_cost_matrix(cost_matrix=matrix)
+
+    for node_index in node_indices:
+        if root.reduced_matrix[root.index][node_index] != INF:
+            child_node = Node.from_parent_node(
+                parent_node=root, node_index=node_index
+            )
+            pq.push(child_node)
+
+    assert not pq.empty
+    assert pq.pop().cost == 25
