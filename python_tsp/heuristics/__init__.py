e5014aeff652f9a265c5d95c8fae9095084a345d
Luan <luan.moraes@loggi.com>
2023-09-04 15:33:32 -0300

    feat: record to record solver

--- python_tsp/heuristics/__init__.py
+++ python_tsp/heuristics/__init__.py
@@ -2,7 +2,7 @@
 ``python_tsp.heuristics``
 =========================
 """
-
 from .lin_kernighan import solve_tsp_lin_kernighan  # noqa: F401
 from .local_search import solve_tsp_local_search  # noqa: F401
+from .record_to_record import solve_tsp_record_to_record  # noqa:
 from .simulated_annealing import solve_tsp_simulated_annealing  # noqa: F401
